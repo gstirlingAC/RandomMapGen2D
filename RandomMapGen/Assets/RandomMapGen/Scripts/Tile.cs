c272a51de1f081beb0d82b59f2859b4e8c0e8a0b
gstirlingAC <gordon.stirling@ayrshire.ac.uk>
2017-05-20 09:42:38 +0100

    Tutorial 2: Creating the tile class
    In this tutorial we create our first class, a Tile class.  In this class we first define a variable to give each tile generated a unique ID, we then create an array to track of all the adjacent tiles.  We finish this tutorial by creating an enum to track the indexes in the array and which side they correspond to.

---
+++ RandomMapGen/Assets/RandomMapGen/Scripts/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 3. We create an enum to help us track each of the indexes
+// in the array and which side they correspond to.
+public enum Sides
+{
+    BOTTOM,
+    RIGHT,
+    LEFT,
+    TOP
+}
+
+public class Tile : MonoBehaviour {
+    // 1. We need to be able to distinguish each tile from one another,
+    // we will therefore need to give them a unique ID. 
+    private int ID = 0;
+
+    // 2. We will also want to keep track of all 
+    // the tiles that are adjacent to this one.
+    private Tile[] neighbours = new Tile[4];
+}
