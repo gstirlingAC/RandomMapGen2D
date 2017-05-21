8d7433c8ab11251e43468882c7030cefa1788391
gstirlingAC <gordon.stirling@ayrshire.ac.uk>
2017-05-21 09:07:38 +0100

    Tutorial 5: Connect the map tester to the map class
    In the previous video we created our RandomMapTester class and we also made a method that allowed us to create a new map. The only problem is that we don't actually have a way of calling it. If you click on the RandomMapTester instance, in the scene, you'll see that there's no way to actually call that method. We're gonna fix that by creating a custom editor and in that editor, we'll create a button that'll allow us to call the method to create a new map. Let's go into our editor folder and create a new C# script.

---
+++ RandomMapGen/Assets/RandomMapGen/Editor/RandomMapTesterEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// 1. Import the Unity Editor namespace. 
+// We need this to create an inspector element.
+using UnityEditor; 
+
+// 2. We add a CustomEditor attribute to associate this class 
+// as a custom editor for the random map tester.
+[CustomEditor(typeof(RandomMapTester))] 
+
+// 3. Replace MonoBehaviour with Editor as we are now extending Editor
+// as opposed to MonoBehaviour.
+public class RandomMapTesterEditor : Editor {
+    // 4. Create an override method for the predefined OnInspectorGUI method.
+    public override void OnInspectorGUI()
+    {
+        // 5. Because we are overriding OnInspectorGUI 
+        // we need to manually call the DrawDefaultInspector method.
+        // This way the inspector should work as before.
+        DrawDefaultInspector(); 
+
+        // 6. Create a variable to store our script and
+        // cast the target to the RandomMapTester class.
+        var script = (RandomMapTester)target;
+
+        // 7. Create a button using GUILayout to generate our "island".
+        if(GUILayout.Button("Generate Island"))
+        {
+            // 8. We need to create a condition to limit the execution
+            // of the MakeMap method to when the game is running only.
+            if (Application.isPlaying)
+            {
+                script.MakeMap(); // 9. Call MakeMap from the script variable.
+            }
+        }
+    }
+}
